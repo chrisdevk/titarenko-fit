698b0fe0bc70bdea017f6734a5c36d8a98d23f19
shadow16305 <chris16305@gmail.com>
2025-01-10 16:30:21 +0100

    Add testimonials

--- app/(frontend)/[locale]/_components/testimonials/testimonial-carousel.tsx
+++ app/(frontend)/[locale]/_components/testimonials/testimonial-carousel.tsx
@@ -6,53 +6,58 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 import { carouselItems } from "@/lib/constants";
+import { useTranslations } from "next-intl";
 import Image from "next/image";
 
 export const TestimonialCarousel = () => {
+  const t = useTranslations("HomePage.testimonials");
+
   return (
     <Carousel className="mt-9 relative">
-      <CarouselContent className="flex">
-        {carouselItems.map((person) => (
+      <CarouselContent className="max-w-full -ml-2 md:-ml-4 pt-12">
+        {carouselItems.map((person, index) => (
           <CarouselItem
             key={person.name}
-            className="bg-[#F6F3FE] rounded-3xl pr-6 py-10 pl-[75px] flex flex-col gap-y-3 relative md:basis-5/12 ml-16"
+            className="basis-full md:basis-1/2 lg:basis-1/3 pl-3 md:pl-4"
           >
-            <div className="absolute left-0 top-1/2 -translate-y-1/2 w-[29px] h-3/4">
-              <Image
-                src="/images/icons/card-decoration.svg"
-                alt="card decoration"
-                fill
-              />
+            <div className="bg-[#F6F3FE] rounded-3xl px-7 md:px-6 pb-5 pt-8 lg:pb-10 lg:pt-20 flex flex-col gap-y-2 lg:gap-y-3 relative h-full">
+              <div className="absolute top-0 left-1/2 -translate-x-1/2 w-[202px] h-[29px]">
+                <Image
+                  src="/images/icons/card-decoration-mobile.svg"
+                  alt="card decoration"
+                  fill
+                />
+              </div>
+              <div className="absolute left-1/2 -translate-x-1/2 -top-12 size-24">
+                <Image
+                  src={person.avatar}
+                  alt={person.name}
+                  fill
+                  className="rounded-full border-2 border-white"
+                />
+              </div>
+              <div className="flex justify-between">
+                <Image
+                  src="/images/icons/stars.svg"
+                  alt="5 star rating"
+                  width={116}
+                  height={20}
+                />
+                <Image
+                  src="/images/icons/quote.svg"
+                  alt="quote"
+                  width={44}
+                  height={36}
+                  className="self-end"
+                />
+              </div>
+              <h4 className="text-lg font-medium">{t(`${index}.name`)}</h4>
+              <p className="text-grey-custom">{t(`${index}.review`)}</p>
             </div>
-            <div className="absolute -left-12 top-1/2 -translate-y-1/2 size-24">
-              <Image
-                src={person.avatar}
-                alt={person.name}
-                fill
-                className="rounded-full border-2 border-white"
-              />
-            </div>
-            <div className="flex justify-between">
-              <Image
-                src="/images/icons/stars.svg"
-                alt="5 star rating"
-                width={116}
-                height={20}
-              />
-              <Image
-                src="/images/icons/quote.svg"
-                alt="quote"
-                width={44}
-                height={36}
-                className="self-end"
-              />
-            </div>
-            <h4 className="text-lg font-medium">{person.name}</h4>
-            <p className="text-grey-custom">{person.review}</p>
           </CarouselItem>
         ))}
       </CarouselContent>
-      <div className="flex gap-x-4 justify-center mt-6 lg:mt-0 lg:absolute lg:right-[6.5%] lg:-top-1/3">
+      <div className="flex gap-x-4 justify-center mt-6 lg:mt-0 lg:absolute lg:right-0 lg:-top-[20%]">
         <CarouselPrevious variant="default" />
         <CarouselNext variant="default" />
       </div>
--- app/(frontend)/[locale]/_components/testimonials/testimonials.tsx
+++ app/(frontend)/[locale]/_components/testimonials/testimonials.tsx
@@ -3,7 +3,7 @@ import { TestimonialCarousel } from "./testimonial-carousel";
 
 export const Testimonials = () => {
   return (
-    <section className="mt-24 lg:mt-[120px] ml-[6%]">
+    <section className="mt-24 lg:mt-[120px] w-11/12 max-w-[1440px] mx-auto">
       <div className="space-y-2.5 max-w-[550px]">
         <span className="bg-violet-500/20 text-violet-900 rounded-3xl px-3 py-2">
           Отзывы
