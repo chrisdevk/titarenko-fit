da6c0ae589f50b57ef48a44467c57be8f13f6acb
shadow16305 <chris16305@gmail.com>
2025-01-19 15:42:31 +0100

    Fix types

--- collections/Users/endpoints/customer.ts
+++ collections/Users/endpoints/customer.ts
@@ -80,10 +80,9 @@ export const customerProxy: PayloadHandler = async (req: PayloadRequest) => {
     if (req.method === "PATCH") {
       if (!req.body) throw new Error("No customer data provided");
       // TODO: lock down the spread `customer` object to only allow certain fields
-      // @ts-expect-error
       response = await stripe.customers.update(
         req.user.stripeCustomerID,
-        req.body
+        req.body as Stripe.CustomerUpdateParams
       );
     }
 
