067ba45d2e24409313583ad30546fa170e705c8b
shadow16305 <chris16305@gmail.com>
2025-03-27 11:11:36 +0100

    change from flex to grid

--- app/(frontend)/[locale]/programs/_components/program-row.tsx
+++ app/(frontend)/[locale]/programs/_components/program-row.tsx
@@ -8,37 +8,46 @@ interface ProgramRowProps {
   locale: string;
 }
 
-export const ProgramRow = ({ programs, locale }: ProgramRowProps) => (
-  <div
-    className={cn(
-      "flex w-full flex-wrap gap-y-5",
-      programs.length === 3 ? "justify-between" : "justify-start gap-x-5",
-    )}
-  >
-    {programs.map((program, index) => {
-      const imgSrc =
-        typeof program.product_thumbnail === "object" &&
-        program.product_thumbnail?.url
-          ? program.product_thumbnail.url
-          : "/images/no-image.webp";
-      return (
-        <motion.div
-          key={program.id}
-          initial={{ opacity: 0, y: 20 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          viewport={{ once: true }}
-          transition={{ duration: 0.3, delay: index * 0.1 }}
-          className="w-[32.5%]"
-        >
-          <ProgramCard
-            title={program.title}
-            question={program.product_question}
-            description={program.product_description!}
-            imgSrc={imgSrc}
-            path={`/${locale}/programs/${program.id}`}
-          />
-        </motion.div>
-      );
-    })}
-  </div>
-);
+export const ProgramRow = ({ programs, locale }: ProgramRowProps) => {
+  const localizedPrograms = programs.filter((program) => program.title?.trim());
+
+  console.log("programs:", programs);
+
+  return (
+    <div
+      className={cn(
+        "grid grid-cols-12 gap-5",
+        localizedPrograms.length === 3
+          ? "justify-between"
+          : "justify-start gap-x-5",
+      )}
+    >
+      {localizedPrograms.map((program, index) => {
+        const imgSrc =
+          typeof program.product_thumbnail === "object" &&
+          program.product_thumbnail?.url
+            ? program.product_thumbnail.url
+            : "/images/no-image.webp";
+
+        return (
+          <motion.div
+            key={program.id}
+            initial={{ opacity: 0, y: 20 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true }}
+            transition={{ duration: 0.3, delay: index * 0.1 }}
+            className="col-span-12 md:col-span-6 lg:col-span-4"
+          >
+            <ProgramCard
+              title={program.title}
+              question={program.product_question}
+              description={program.product_description!}
+              imgSrc={imgSrc}
+              path={`/${locale}/programs/${program.id}`}
+            />
+          </motion.div>
+        );
+      })}
+    </div>
+  );
+};
